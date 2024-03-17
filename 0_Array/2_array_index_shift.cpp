005073c449a4df51fc2d938524cacea2525578b1
siyam-hossain <siyam.cybersoul@gmail.com>
2024-03-17 15:58:44 +0600

    Update 2_array_index_shift.cpp

--- 0_Array/2_array_index_shift.cpp
+++ 0_Array/2_array_index_shift.cpp
@@ -27,9 +27,13 @@ int main(int argc, char const *argv[])
     return 0;
 }
 /*
-    The issue with the code is that it is using 1-based indexing for the array arr. In C++, arrays are 0-based, which means the first element of the array is at index 0.
-
-    In the code, you are taking input for n elements of the array arr using a for loop that starts from index 1 and goes up to n. This means that the first n elements of the array are not initialized, and their values are undefined.
-
-    When you try to print arr[0], you are accessing an uninitialized element of the array, which can lead to unpredictable results. In this case, it seems like the value of arr[0] is 3, but this is just a coincidence and can change with different compilers or runs of the program.
+-The issue with the code is that it is using 1-based indexing for the array arr. 
+-In C++, arrays are 0-based, which means the first element of the array is at index 0.
+-In the code, you are taking input for n elements of the array arr using a for loop.
+-Itstarts from index 1 and goes up to n. 
+-This means that the first n elements of the array are not initialized, and their values are undefined.
+-When you try to print arr[0], 
+-you are accessing an uninitialized element of the array, which can lead to unpredictable results. 
+-In this case, it seems like the value of arr[0] is 3. 
+-But this is just a coincidence and can change with different compilers or runs of the program.
 */
